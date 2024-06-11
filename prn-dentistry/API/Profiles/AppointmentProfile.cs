ff485071ba7609f66cc5699b0d88cadd1db7612f
Võ Minh Hoàng <vominhhoangkhung@gmail.com>
2024-06-11 08:35:07 +0700

    update

--- prn-dentistry/API/Profiles/AppointmentProfile.cs
+++ prn-dentistry/API/Profiles/AppointmentProfile.cs
@@ -9,9 +9,11 @@ namespace prn_dentistry.API.Profiles
   {
     public AppointmentProfile()
     {
-      CreateMap<AppointmentDto, Appointment>().ReverseMap();
+      CreateMap<AppointmentDto, Appointment>().ReverseMap()
+        .ForMember(dest => dest.Address, opt => opt.MapFrom(src => src.Customer.Address))
+        .ForMember(dest => dest.PhoneNumber, opt => opt.MapFrom(src => src.Customer.PhoneNumber));
       CreateMap<AppointmentCreateDto, Appointment>().ReverseMap();
       CreateMap<AppointmentUpdateDto, Appointment>().ReverseMap();
     }
   }
-}
+} 
