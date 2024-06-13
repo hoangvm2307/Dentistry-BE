67b8267d1ac53e9f21386682d27b6f9b7dfea4ef
Minh Khoi <62336255+MarkVoVN@users.noreply.github.com>
2024-06-13 09:45:10 +0700

    Added deployment config (#8)
    * commit
    
    * update vol config
    
    * update
    
    * update
    
    * upadte
    
    * update
    
    * .
    
    * .
    
    * .
    
    * update
    
    * multiple dockerfile
    
    * update
    
    * remove bin and obj dir, update git ignore

--- prn-dentistry/Program.cs
+++ prn-dentistry/Program.cs
@@ -14,6 +14,7 @@ using prn_dentistry.API.Extensions;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
