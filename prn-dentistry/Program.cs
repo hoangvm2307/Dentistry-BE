993c74a50053d80ecef032441a16d597569ef36a
Võ Minh Hoàng <vominhhoangkhung@gmail.com>
2024-07-04 16:22:26 +0700

    update

--- prn-dentistry/Program.cs
+++ prn-dentistry/Program.cs
@@ -88,7 +88,7 @@ builder.Services.AddSingleton(provider =>
 var app = builder.Build();
 FirebaseApp.Create(new AppOptions()
 {
-  Credential = GoogleCredential.FromJson(Environment.GetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS_JSON"))
+ Credential = GoogleCredential.FromJson(Environment.GetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS_JSON"))
 });
   Console.WriteLine(Environment.GetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS_JSON"));
 // Configure the HTTP request pipeline.
