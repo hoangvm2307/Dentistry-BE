f3131ec347ee79586fb837c14090081b57611261
Vo Minh Khoi <vominhkhoi12032003@gmail.com>
2024-07-04 14:33:21 +0700

    copy config

--- Firebase/FirebaseStorageService.cs
+++ Firebase/FirebaseStorageService.cs
@@ -2,14 +2,14 @@
 
 namespace Firebase
 {
-  public class FirebaseStorageService :IFirebaseStorageService
+  public class FirebaseStorageService : IFirebaseStorageService
   {
     private readonly StorageClient _storageClient;
     private readonly string _bucketName;
 
     public FirebaseStorageService(string bucketName)
     {
-      
+
       _storageClient = StorageClient.Create();
       _bucketName = bucketName;
     }
@@ -19,22 +19,22 @@ namespace Firebase
       var contentType = "image/jpeg";
       await _storageClient.UploadObjectAsync(_bucketName, objectName, contentType, fileStream);
 
-    //   var objectUrl = GetPublicUrl(_bucketName, objectName);
-        var objectUrl = $"https://storage.googleapis.com/{_bucketName}/{objectName}";
+      //   var objectUrl = GetPublicUrl(_bucketName, objectName);
+      var objectUrl = $"https://storage.googleapis.com/{_bucketName}/{objectName}";
       return objectUrl;
 
     }
     private string GetPublicUrl(string bucket, string objectName)
     {
       var serviceAccountPath = Environment.GetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS");
-
+      Console.WriteLine('#####################################################', serviceAccountPath);
       if (string.IsNullOrEmpty(serviceAccountPath) || !File.Exists(serviceAccountPath))
       {
         throw new InvalidOperationException("Service account key file not found. Ensure GOOGLE_APPLICATION_CREDENTIALS is set correctly.");
       }
 
       var urlSigner = UrlSigner.FromServiceAccountPath(serviceAccountPath);
-      
+
       var url = urlSigner.Sign(bucket, objectName, TimeSpan.FromDays(7));
 
 
