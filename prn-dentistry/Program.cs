45fce0659893c0b419ab3a994d6f92f7bdb9caca
Võ Minh Hoàng <vominhhoangkhung@gmail.com>
2024-07-07 16:39:38 +0700

    update

--- prn-dentistry/Program.cs
+++ prn-dentistry/Program.cs
@@ -103,6 +103,7 @@ app.UseHttpsRedirection();
 app.UseCors(opt =>
 {
   opt.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins("http://localhost:3000");
+  opt.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins("https://dentistry.api.markvoit.id.vn");
 });
 app.UseAuthentication();
 app.UseAuthorization();
