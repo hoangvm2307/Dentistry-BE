5ec64723fb13b0172bb256cd771fea7f8f7acfc8
Võ Minh Hoàng <vominhhoangkhung@gmail.com>
2024-07-14 00:27:25 +0700

    update

--- prn-dentistry/API/Profiles/AppointmentProfile.cs
+++ prn-dentistry/API/Profiles/AppointmentProfile.cs
@@ -13,7 +13,9 @@ namespace prn_dentistry.API.Profiles
         .ForMember(dest => dest.Address, opt => opt.MapFrom(src => src.Dentist.Clinic.Address))
         .ForMember(dest => dest.ClinicPhoneNumber, opt => opt.MapFrom(src => src.Dentist.Clinic.PhoneNumber))
         .ForMember(dest => dest.DentistPhoneNumber, opt => opt.MapFrom(src => src.Dentist.PhoneNumber))
-        .ForMember(dest => dest.Price, opt => opt.MapFrom(src => src.Service.Price));
+        .ForMember(dest => dest.Price, opt => opt.MapFrom(src => src.Service.Price))
+        .ForMember(dest => dest.ClinicID, opt => opt.MapFrom(src => src.Dentist.Clinic.ClinicID));
+
 
       CreateMap<AppointmentCreateDto, Appointment>().ReverseMap();
       CreateMap<AppointmentUpdateDto, Appointment>().ReverseMap();
