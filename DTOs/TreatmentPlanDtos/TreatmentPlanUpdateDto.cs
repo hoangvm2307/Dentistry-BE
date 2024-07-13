ae58f314868a0def2ab254d0955b10a48bf3a889
Võ Minh Hoàng <vominhhoangkhung@gmail.com>
2024-07-13 22:17:13 +0700

    update

--- DTOs/TreatmentPlanDtos/TreatmentPlanUpdateDto.cs
+++ DTOs/TreatmentPlanDtos/TreatmentPlanUpdateDto.cs
@@ -9,13 +9,13 @@ namespace DTOs.TreatmentPlanDtos
     [CustomDateRange(ErrorMessage = "Start Date cannot be in the past.")]
     public DateTime StartDate { get; set; }
 
-    [DateGreaterThan("StartDate", ErrorMessage = "End Date must be greater than Start Date.")]
-    public DateTime EndDate { get; set; }
+ 
+    public DateTime? EndDate { get; set; }
 
     [MaxLength(50, ErrorMessage = "Frequency cannot exceed 50 characters.")]
     public string Description { get; set; }
 
-    public DateTime NextAppointmentDate { get; set; }
+    public DateTime? NextAppointmentDate { get; set; }
 
     [MaxLength(50, ErrorMessage = "Status cannot exceed 50 characters.")]
     public string Status { get; set; }
